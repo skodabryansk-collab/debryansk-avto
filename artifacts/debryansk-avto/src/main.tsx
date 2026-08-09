0d5dc26807e042cefe57dcf504bb48c27dc5e73f
skodabryansk-collab <skodabryansk@gmail.com>
2026-08-09 17:46:08 +0300

    task-424: centralize MotionConfig reducedMotion=user at root; guard FadeIn/modal/cookiebanner motion; fix buyout import

--- artifacts/debryansk-avto/src/main.tsx
+++ artifacts/debryansk-avto/src/main.tsx
@@ -1,10 +1,13 @@
 import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
+import { MotionConfig } from "framer-motion";
 import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <HelmetProvider>
-    <App />
+    <MotionConfig reducedMotion="user">
+      <App />
+    </MotionConfig>
   </HelmetProvider>
 );
